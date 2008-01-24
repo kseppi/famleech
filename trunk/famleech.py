e4a0f1a602f8bdcea04e2208c2bde14f032cad66
jlcarroll <jlcarroll@9269472e-7c44-0410-9e20-4bbd1ceb7664>
2008-01-24 18:53:01 +0000

    This is version 0.1

---
+++ trunk/famleech.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import httplib
+import getopt
+import sys
+from collections import deque
+import htmllib, formatter, urlparse
+from random import random
+
+
+def leech(url):
+	scheme, netloc, path, params, query, fragment = urlparse.urlparse(url)
+	if scheme != 'http':
+		print 'Malformed URL'
+		sys.exit(-1)
+	
+	url = path + '?' + params + query
+	slash = url.rindex('/')
+	directory = url[:slash+1]
+	file = url[slash+1:]
+	conn = httplib.HTTPConnection(netloc)
+	queue = deque([url])
+	
+	class Parser(htmllib.HTMLParser):
+		def start_a(self, attrs):
+			ref = dict(attrs)['href']
+			if ref.startswith('pedigree_view.asp') or \
+					ref.startswith('pedigree_chart_gedcom.asp'):
+				queue.appendleft(directory + ref)
+	
+	parser = Parser(formatter.NullFormatter())
+	
+	count = 0
+	while len(queue) > 0:
+		url = queue.pop()
+		print 'Requesting', url
+		conn.request("GET", url)
+		while True:
+			delay = 5 * random()
+			sys.sleep(delay)
+			try:
+				resp = conn.getresponse()
+				respSuccess=true
+				break
+			except httplib.HTTPException, e:
+				respSuccess=false
+				print "Got an exception while downloading \"%s\"" % url
+				print e, repr(e)
+				print "Waiting to retry (hit CTRL-c to skip)"
+				try:
+					sys.sleep(60)
+				except KeyboardInterrupt:
+					print "Giving up"
+					break
+				print "Retrying"
+		if respSuccess==true:
+			data = resp.read()
+			if 'pedigree_chart_gedcom.asp' in url:
+				f = open('%04d.ged' % count, 'wb')
+				f.write(data)
+				f.close()
+				count = count + 1
+			parser.feed(data)
+			parser.close()
+	    
+	conn.close()
+
+
+if __name__ == '__main__':
+	opts, args = getopt.getopt(sys.argv[1:], '')
+	if len(args) != 1:
+		print 'Usage: famleech.py <url>'
+		sys.exit(-1)
+	
+	leech(args[0])
